89e15c91b26d20e02ef88685714a66ef2b118d2b
JuanMartinCoder <skyzinformatica@gmail.com>
2019-11-19 19:14:20 -0300

    cambie ip del servidor

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/',function(req,res){
     res.sendFile( __dirname + "/" + "index.html" );
 })
 
-app.listen(3030, function () {
+app.listen('3030','192.168.0.105', function () {
     console.log('Example app listening on port 3000!');
   });
